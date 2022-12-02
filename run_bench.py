285536c06945f155ab8618a7eb51f52503e590dc
Calvin Gealy <c@gealy.co>
2022-12-02 00:09:40 -0500

    think I fixed the remote ssh stuff... hopefully

--- run_bench.py
+++ run_bench.py
@@ -94,8 +94,8 @@ def main():
     set_kernel_params(general_conf["kernel_params"], server_hostname, server_pw_path)
 
     has_veth, virtual_interface = "virtual_interface" in exp_conf, exp_conf.get("virtual_interface")
-    #set_netem(server_hostname, server_pw_path, server_ip, interface, 
-        #server_ingress_interface, exp_conf["netem_conf"], virtual_interface)
+    set_netem(server_hostname, server_pw_path, server_ip, interface, 
+        server_ingress_interface, exp_conf["netem_conf"], virtual_interface)
     
     test_rtt(server_ip)
     test_bandwidth(server_hostname, server_ip)
@@ -182,8 +182,8 @@ def main():
                     subprocess.run(["sudo", "ip", "link", "set", "dev", interface, "down"], check=True)
                     subprocess.run(["sudo", "ip", "link", "set", "dev", interface, "up"], check=True)
                     clear_netem(server_hostname, server_pw_path, server_ip, interface, server_ingress_interface, virtual_interface)
-                    #set_netem(server_hostname, server_pw_path, server_ip, interface, 
-                        #server_ingress_interface, exp_conf["netem_conf"], virtual_interface)
+                    set_netem(server_hostname, server_pw_path, server_ip, interface, 
+                        server_ingress_interface, exp_conf["netem_conf"], virtual_interface)
 
                     # kill processes
                     subprocess.run(get_remote_cmd(server_hostname, ["pkill", "tcpdump"]))
--- utils/remote_cmd.py
+++ utils/remote_cmd.py
@@ -3,7 +3,7 @@ def get_remote_cmd(remote_hostname, cmd_lst):
     return ["ssh", remote_hostname, " ".join(cmd_lst)]
 
 def get_remote_cmd_sudo(remote_hostname, remote_pw_path, cmd):
-    return "echo {} && ssh {} cat \| sudo --prompt="" -S -- \"{}\"".format(remote_pw_path, remote_hostname, cmd)
+    return "ssh {} 'sudo {}'".format(remote_hostname, cmd)
 
 def get_scp_file_to_remote_cmd(remote_hostname, local_file_path, remote_dir):
     return ["scp", local_file_path, "{}:{}".format(remote_hostname, remote_dir)]
